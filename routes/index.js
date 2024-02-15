edb77b461c4ffe91f2fc6f6ce70afac51dcaa3ca
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2024-02-15 16:17:32 +0900

    cors add

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
+
+var corsOptions = {
+  origin: process.env.CORS_ORIGIN || 'http://localhost',
+  optionsSuccessStatus: 200
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', cors(corsOptions), function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
